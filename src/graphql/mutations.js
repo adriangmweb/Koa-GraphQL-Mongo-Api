8628332bf64e3e4c3281d3378372804cfb624c0a
Adrian GM <adriangmweb@gmail.com>
2020-03-21 13:07:06 +0000

    add update mutation

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -22,6 +22,24 @@ const Mutation = new GraphQLObjectType({
         })
         return newGadget.save();
       }
+    },
+    updateGadget: {
+      type: gadgetGraphQLType,
+      args: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        release_date: { type: GraphQLString },
+        by_company: { type: GraphQLString },
+        price: { type: GraphQLString }
+      },
+      async resolve(parent, args) {
+        const gadget = await Gadget.findById(args.id)
+        gadget.name = args.name,
+        gadget.release_date = args.release_date
+        gadget.by_company = args.by_company
+        gadget.price = args.price
+        return gadget.save()
+      }
     }
   }
 })
